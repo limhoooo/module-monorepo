1bee708cae22df4126ddf6f3d9d604df2c405794
lim ho <dlagh123@gmail.com>
2023-09-21 15:00:50 +0900

    component-h 리팩토링 및 함수 추가
    - element -> jsx 변경하는 함수 추가 (changeJsx)
    - updateElement 함수 관심사 분리
    - 노드 간 key 로 비교하게끔 변경

--- packages/component-h/index.js
+++ packages/component-h/index.js
@@ -1,4 +1,4 @@
-import { updateElement } from './core/updateElement.js';
+import { updateElement, changeJsx } from './core/parserElement.js';
 export default class Component {
   constructor(target, props, route) {
     this.target = document.querySelector(target);
@@ -29,17 +29,12 @@ export default class Component {
   }
   // 랜더링
   render() {
-    requestAnimationFrame(() => {
-      const newNode = this.target.cloneNode(true);
-      newNode.innerHTML = this.template().outerHTML;
-      // DIFF알고리즘 적용
-      const oldChildNodes = [...this.target.childNodes];
-      const newChildNodes = [...newNode.childNodes];
-      const max = Math.max(oldChildNodes.length, newChildNodes.length);
-      for (let i = 0; i < max; i++) {
-        updateElement(this.target, newChildNodes[i], oldChildNodes[i]);
-      }
-      this.onMounted();
-    });
+    // console.log('render');
+    updateElement(
+      this.target,
+      this.template(),
+      changeJsx(this.target).children[0],
+    );
+    this.onMounted();
   }
 }
