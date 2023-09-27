f8e37aa30ca479ee888fe78bdf0debd84fc17078
lim ho <dlagh123@gmail.com>
2023-09-27 10:21:24 +0900

    Fetch 리팩토링
    - timeout 기능 추가
    - FormData 분기처리 로직 추가
    - 객체를 스트링으로 body 직렬화
    - GET 요청시 쿼리파라미터 & 쿼리스트링 파싱
    - API 요청시 headers 변경

--- packages/fetch-h/index.js
+++ packages/fetch-h/index.js
@@ -1,29 +1,40 @@
-// url 받아올수있어야함
-// base url 지정할수있어야함
-// method (GET,POST,PUT,DELETE) 인자로 받아야함
-// headers 도 설정가능해야함
-// 에러처리
-
 export default class Fetch {
-  constructor(baseUrl, headers) {
-    this.baseUrl = baseUrl;
-    this.headers = {
-      'Content-Type': 'application/json; charset=utf-8',
+  #baseUrl = null;
+  #headers = {};
+  #timeout = null;
+  constructor(baseUrl, headers, time) {
+    this.#baseUrl = baseUrl;
+    this.#headers = {
       ...headers,
     };
+    this.#timeout = time;
   }
 
-  async httpRequest(method, url, payload) {
+  async #httpRequest(method, url, payload, headers) {
     try {
+      // Timeout 기능
+      const controller = new AbortController();
+      const signal = controller.signal;
+      setTimeout(() => {
+        controller.abort();
+      }, this.#timeout);
+
       const options = {
         method,
-        headers: this.headers,
+        headers: headers ? headers : this.#headers,
+        signal,
       };
-      if (method !== 'GET') {
-        options.body = JSON.stringify(payload);
+      if (payload) {
+        // FormData 일시 직렬화 X
+        if (payload instanceof FormData) {
+          options.body = payload;
+        } else {
+          // 객체를 스트링으로 body 직렬화
+          options.body = JSON.stringify(payload);
+        }
       }
-      const response = await fetch(this.baseUrl + url, options);
 
+      const response = await fetch(this.#baseUrl + url, options);
       if (!response.ok) {
         throw new Error(`Error status: ${response.status}`);
       }
@@ -31,24 +42,93 @@ export default class Fetch {
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error(error);
-      throw error;
+      if (error.name === 'AbortError') {
+        // TimeOut Error
+        throw new Error(`Timeout Error: ${error}`);
+      } else {
+        // Network Error
+        throw new Error(`Network Error: ${error}`);
+      }
     }
+  }
+
+  get(url, data, headers) {
+    const parseUrl = data ? parseUrlAndParam(url, data) : url;
+    return this.#httpRequest('GET', parseUrl, null, headers);
+  }
+  post(url, data, headers) {
+    return this.#httpRequest('POST', url, data, headers);
+  }
+  put(url, data, headers) {
+    return this.#httpRequest('PUT', url, data, headers);
+  }
+  patch(url, data, headers) {
+    return this.#httpRequest('PATCH', url, data, headers);
+  }
+  delete(url, headers) {
+    return this.#httpRequest('DELETE', url, null, headers);
   }
 }
 
-// async function test() {
-//   const fetchApi = new fetch('https://jsonplaceholder.typicode.com', {
-//     Authorization: 'aaa',
-//   });
+function parseUrlAndParam(url, data) {
+  // 쿼리파라미터 & 쿼리스트링 파싱
+  if (!data) return url;
 
-//   const aaa = await fetchApi.httpRequest('GET', '/todos/1');
-//   const bbb = await fetchApi.httpRequest('POST', '/posts', {
-//     title: 'foo',
-//     body: 'bar',
-//     userId: 1,
-//   });
-//   console.log(aaa);
-//   console.log(bbb);
-// }
-// test();
+  if (data !== null && typeof data === 'object') {
+    const queryString = objectToQueryString(data);
+    return `${url}?${queryString}`;
+  } else {
+    return `${url}/${data}`;
+  }
+}
+export function objectToQueryString(obj) {
+  const queryParams = [];
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const encodedKey = encodeURIComponent(key);
+      const encodedValue = encodeURIComponent(obj[key]);
+
+      queryParams.push(`${encodedKey}=${encodedValue}`);
+    }
+  }
+
+  return queryParams.join('&');
+}
+/////////////////////////////////////////////////////////////////
+
+// 별도 파일 분리
+// ex : http.js
+const instanceA = new Fetch(
+  'https://jsonplaceholder.typicode.com',
+  {
+    'Content-Type': 'application/json; charset=utf-8',
+  },
+  5000,
+);
+const instanceB = new Fetch(
+  'https://test.co.kr',
+  {
+    'Content-Type': 'application/json; charset=utf-8',
+  },
+  5000,
+);
+
+// 별도 파일 분리
+// ex : /api/todo.js
+// API 별 export 해서 사용
+export const getTodos = () => instanceA.get(`/posts`);
+export const postTodos = data => instanceA.post(`/posts`, data);
+
+// 호출하는부분
+async function test() {
+  const obj = {
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  };
+  const todoList = await getTodos();
+  console.log(todoList);
+  await postTodos(obj);
+}
+test();
