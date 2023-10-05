47a35309c4030545c2118a9a711f4033c46fdcda
lim ho <dlagh123@gmail.com>
2023-10-05 13:29:28 +0900

    Fetch 리팩토링
    - Fetch 생성시 인자로 받던 baseUrl, Headers, Timeout 별도 메소드로 분리
    - httpRequest 함수 관심사 분리
    - response 리턴 객체 생성
    - error 리턴 객체 생성
    - 메소드 인자 객체형태로 변경
    - get 메소드 호출시 쿼리파라미터 배열 파싱 추가

--- packages/fetch-h/index.js
+++ packages/fetch-h/index.js
@@ -1,88 +1,116 @@
 export default class Fetch {
-  #baseUrl = null;
+  #baseUrl = '';
   #headers = {};
-  #timeout = null;
-  constructor(baseUrl, headers, time = 5000) {
-    this.#baseUrl = baseUrl;
-    this.#headers = {
-      ...headers,
-    };
-    this.#timeout = time;
+  #timeout = 5000;
+  constructor() {
+    this.#baseUrl = '';
+    this.#headers = {};
+    this.#timeout = 5000;
   }
 
-  async #httpRequest(method, url, payload, headers) {
+  async #httpRequest({ method, url, payload, headers }) {
     try {
-      // Timeout 기능
-      const controller = new AbortController();
-      const signal = controller.signal;
-      setTimeout(() => {
-        controller.abort();
-      }, this.#timeout);
-
+      const signal = this.#setRequestTimeout();
       const options = {
         method,
         headers: headers ? headers : this.#headers,
         signal,
       };
-      if (payload) {
-        // FormData 일시 직렬화 X
-        if (payload instanceof FormData) {
-          options.body = payload;
-        } else {
-          // 객체를 스트링으로 body 직렬화
-          options.body = JSON.stringify(payload);
-        }
-      }
+
+      if (payload) options.body = parsePayload(payload);
+
       const parseurl = this.#baseUrl ? this.#baseUrl + url : url;
       const response = await fetch(parseurl, options);
       if (!response.ok) {
-        throw new Error(`Error status: ${response.status}`);
+        return this.#handleResponse({
+          status: 'fail',
+          statusCode: response.status,
+        });
       }
-
       const data = await response.json();
-      return data;
+      return this.#handleResponse({
+        status: 'success',
+        statusCode: response.status,
+        data,
+      });
     } catch (error) {
-      if (error.name === 'AbortError') {
-        // TimeOut Error
-        throw new Error(`Timeout Error: ${error}`);
-      } else {
-        // Network Error
-        throw new Error(`Network Error: ${error}`);
-      }
+      return this.#handleRequestError(error);
     }
   }
 
-  get(url, data, headers) {
-    const parseUrl = data ? parseUrlAndParam(url, data) : url;
-    console.log(parseUrl);
-    return this.#httpRequest('GET', parseUrl, null, headers);
+  #handleResponse({ status, statusCode, data }) {
+    return {
+      status,
+      statusCode,
+      response: data,
+    };
   }
-  post(url, data, headers) {
-    return this.#httpRequest('POST', url, data, headers);
+  #handleRequestError(error) {
+    return {
+      status: 'fail',
+      errorName: error.name,
+      errorMsg: error,
+    };
   }
-  put(url, data, headers) {
-    return this.#httpRequest('PUT', url, data, headers);
+  setBaseUrl(baseUrl) {
+    this.#baseUrl = baseUrl;
   }
-  patch(url, data, headers) {
-    return this.#httpRequest('PATCH', url, data, headers);
+  setHeaders(headers) {
+    this.#headers = headers;
   }
-  delete(url, headers) {
-    return this.#httpRequest('DELETE', url, null, headers);
+  #setRequestTimeout() {
+    // Timeout 기능
+    const controller = new AbortController();
+    setTimeout(() => {
+      controller.abort();
+    }, this.#timeout);
+    return controller.signal;
+  }
+  setHeadersConfig(config) {
+    if (Array.isArray(config)) {
+      for (const { name, data } of config) {
+        this.#headers[name] = data;
+      }
+    } else {
+      this.#headers[config.name] = config.data;
+    }
+  }
+
+  setTimeout(timeout) {
+    this.#timeout = timeout;
+  }
+  get({ url, param, headers }) {
+    const parseUrl = param ? parseUrlAndParam({ url, param }) : url;
+    return this.#httpRequest({ method: 'GET', url: parseUrl, headers });
+  }
+  post({ url, payload, headers }) {
+    return this.#httpRequest({ method: 'POST', url, payload, headers });
+  }
+  put({ url, payload, headers }) {
+    return this.#httpRequest({ method: 'PUT', url, payload, headers });
+  }
+  patch({ url, payload, headers }) {
+    return this.#httpRequest({ method: 'PATCH', url, payload, headers });
+  }
+  delete({ url, headers }) {
+    return this.#httpRequest({ method: 'DELETE', url, headers });
   }
 }
 
-function parseUrlAndParam(url, data) {
+function parseUrlAndParam({ url, data }) {
   // 쿼리파라미터 & 쿼리스트링 파싱
   if (!data) return url;
 
   if (data !== null && typeof data === 'object') {
-    const queryString = objectToQueryString(data);
+    const queryString = Array.isArray(data)
+      ? parseArrayToQueryString(data)
+      : parseObjectToQueryString(data);
     return `${url}?${queryString}`;
   } else {
     return `${url}/${data}`;
   }
 }
-export function objectToQueryString(obj) {
+function parseObjectToQueryString(obj) {
   const queryParams = [];
 
   for (const key in obj) {
@@ -96,40 +124,70 @@ export function objectToQueryString(obj) {
 
   return queryParams.join('&');
 }
+function parseArrayToQueryString(arr) {
+  return arr.join('&');
+}
+function parsePayload(payload) {
+  if (payload instanceof FormData) {
+    // FormData 일시 직렬화 X
+    return payload;
+  } else {
+    // 객체를 스트링으로 body 직렬화
+    return JSON.stringify(payload);
+  }
+}
+
 /////////////////////////////////////////////////////////////////
 
-// // 별도 파일 분리
-// // ex : http.js
-// const instanceA = new Fetch(
-//   'https://jsonplaceholder.typicode.com',
-//   {
-//     'Content-Type': 'application/json; charset=utf-8',
-//   },
-//   5000,
-// );
-// const instanceB = new Fetch(
-//   'https://test.co.kr',
-//   {
-//     'Content-Type': 'application/json; charset=utf-8',
-//   },
-//   5000,
-// );
+// class InitTodoFetch extends Fetch {
+//   constructor() {
+//     super();
+//     this.setBaseUrl('https://jsonplaceholder.typicode.com');
+//     this.setTimeout(5000);
+//     this.setHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
+//     this.setHeadersConfig([
+//       {
+//         name: 'Authorization',
+//         data: 'Bearer a444sdvsadvklasdvjlaksdvjlkasdvjlk',
+//       },
+//       {
+//         name: 'Cookie',
+//         data: 'sessionid=YOUR_SESSION_ID; user_id=12345',
+//       },
+//       {
+//         name: 'Accept',
+//         data: 'application/json',
+//       },
+//     ]);
+//   }
+// }
 
-// // 별도 파일 분리
-// // ex : /api/todo.js
-// // API 별 export 해서 사용
-// export const getTodos = () => instanceA.get(`/posts`);
-// export const postTodos = data => instanceA.post(`/posts`, data);
+// const instance = new InitTodoFetch();
+// const getTodos = () =>
+//   instance.get({
+//     url: '/todos',
+//   });
+// const getTodosParam = (data, obj) =>
+//   instance.get({
+//     url: '/todos',
+//     param: data,
+//   });
+// const postTodo = data =>
+//   instance.post({
+//     url: '/todos',
+//     payload: data,
+//   });
 
-// // 호출하는부분
-// async function test() {
+// async function app() {
 //   const obj = {
 //     title: 'foo',
 //     body: 'bar',
 //     userId: 1,
 //   };
-//   const todoList = await getTodos();
-//   console.log(todoList);
-//   await postTodos(obj);
+//   const queryParamsArray = ['param1=value1', 'param2=value2', 'param3=value3'];
+
+//   console.log(await getTodos());
+//   console.log(await getTodosParam(queryParamsArray, obj));
+//   console.log(await postTodo(obj));
 // }
-// test();
+// app();
