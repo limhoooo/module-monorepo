bcf17b189946d77f37a06d16520f5fa4f43049a1
lim ho <dlagh123@gmail.com>
2023-10-23 09:56:53 +0900

    스타일 컴포넌트 모듈화
    - button
    - input (checkbox)
    - typography
    - icon

---
+++ packages/next-commerce/src/styles/theme.tsx
@@ -0,0 +1,16 @@
+type TypeFontSizeKey = "xxl" | "xl" | "l" | "m" | "s" | "xs" | "xxs";
+
+export const fontSize: Record<TypeFontSizeKey, string> = {
+  xxl: `26px`,
+  xl: `22px`,
+  l: `20px`,
+  m: `18px`,
+  s: `16px`,
+  xs: `14px`,
+  xxs: `12px`,
+};
+export const fontWeight: Record<string, string> = {
+  bold: "700",
+  semibold: "600",
+  regular: "400",
+};
