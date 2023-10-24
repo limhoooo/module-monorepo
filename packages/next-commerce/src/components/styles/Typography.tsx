434a197bd2bd41855922bfeedfd49e63d74f6c30
lim ho <dlagh123@gmail.com>
2023-10-24 16:01:54 +0900

    Typography 컴포넌트 리팩토링
    - 공통으로 쓰일수 있는 style 을 theme 로 옮김
    - Typography 컴포넌트에 불필요한 코드 제거
    - home 컴포넌트에서 실사용 코드 작성

--- packages/next-commerce/src/components/styles/Typography.tsx
+++ packages/next-commerce/src/components/styles/Typography.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { fontSize, fontWeight } from '../../styles/theme';
+import { _typography } from '../../styles/Typography';
+import { TypeFontStyle, TypeFontWeight } from '@/src/styles/theme';
 
 /**
  * 1. global theme 적용 / css in js, scss
@@ -18,20 +19,13 @@ import { fontSize, fontWeight } from '../../styles/theme';
  *```
  */
 
-type TypeColor = {
-  blue_900: string;
-};
-const colors: TypeColor = {
-  blue_900: '#05285B',
-};
-
 type TypeTypo =
-  | 'heading_1'
-  | 'heading_2'
-  | 'heading_3'
-  | 'heading_4'
-  | 'heading_5'
-  | 'heading_6'
+  | 'heading1'
+  | 'heading2'
+  | 'heading3'
+  | 'heading4'
+  | 'heading5'
+  | 'heading6'
   | 'text_xxl'
   | 'text_xl'
   | 'text_l'
@@ -40,99 +34,10 @@ type TypeTypo =
   | 'text_xs'
   | 'text_xxs';
 
-type TypeFontStyle = 'normal' | 'italic' | 'oblique' | 'revert';
-
-type TypeFontWeight = 'bold' | 'semibold' | 'regular';
-
-const heading_1 = styled.h1`
-  font-weight: 500;
-  font-size: 80px;
-  line-height: 84px;
-  letter-spacing: -3px;
-`;
-const heading_2 = styled.h2`
-  font-weight: 500;
-  font-size: 72px;
-  line-height: 76px;
-  letter-spacing: -2px;
-`;
-const heading_3 = styled.h3`
-  font-weight: 500;
-  font-size: 54px;
-  line-height: 58px;
-  letter-spacing: -1px;
-`;
-
-const heading_4 = styled.h4`
-  font-weight: 500;
-  font-size: 40px;
-  line-height: 44px;
-  letter-spacing: -0.4px;
-`;
-
-const heading_5 = styled.h5`
-  font-weight: 500;
-  font-size: 34px;
-  line-height: 38px;
-  letter-spacing: -0.6px;
-`;
-
-const heading_6 = styled.h6`
-  font-weight: 500;
-  font-size: 28px;
-  line-height: 34px;
-  letter-spacing: -0.6px;
-`;
-
-const text_xxl = styled.p`
-  font-size: ${fontSize.xxl};
-  line-height: 40px;
-`;
-const text_xl = styled.p`
-  font-size: ${fontSize.xl};
-  line-height: 34px;
-`;
-const text_l = styled.p`
-  font-size: ${fontSize.l};
-  line-height: 32px;
-`;
-const text_m = styled.p`
-  font-size: ${fontSize.m};
-  line-height: 30px;
-`;
-const text_s = styled.p`
-  font-size: ${fontSize.s};
-  line-height: 26px;
-`;
-const text_xs = styled.p`
-  font-size: ${fontSize.xs};
-  line-height: 22px;
-`;
-const text_xxs = styled.p`
-  font-size: ${fontSize.xxs};
-  line-height: 20px;
-`;
-
-const Text: { [key: string]: any } = {
-  heading_1,
-  heading_2,
-  heading_3,
-  heading_4,
-  heading_5,
-  heading_6,
-  text_xxl,
-  text_xl,
-  text_l,
-  text_m,
-  text_s,
-  text_xs,
-  text_xxs,
-};
-
 type TypographyProps = {
   tag?: string;
   type?: TypeFontStyle;
-  color?: string | TypeColor;
+  color?: string;
   weight?: TypeFontWeight;
   typo: TypeTypo;
   children: React.ReactNode;
@@ -146,10 +51,8 @@ const Typography = ({
   typo,
   type,
 }: TypographyProps) => {
-  const Element: any = styled(Text[typo])`
-    font-style: ${({ type }) => type};
-    font-weight: ${({ weight }) => fontWeight[weight]};
-    color: ${({ color }) => color};
+  const Element: any = styled.span`
+    ${_typography[typo]}
   `;
   return (
     <Element as={tag} color={color} typo={typo} type={type} weight={weight}>
--- packages/next-commerce/src/pages/home.tsx
+++ packages/next-commerce/src/pages/home.tsx
@@ -15,55 +15,16 @@ const home = () => {
   };
   return (
     <div>
-      <Checkbox
-        size={'l'}
-        name={'test1'}
-        onChange={onChange}
-        label={'클릭해주세요'}
-      />
-      <Checkbox
-        size={'m'}
-        name={'test2'}
-        onChange={onChange}
-        label={'클릭해주세요'}
-      />
-      home
-      <Button size={'xl'} onClick={clickTest}>
-        Xlarge
-        <Icon size={'l'} icon={'arrow-right'} color={'white'} />
-      </Button>
-      <Typography typo={'heading_1'} type={'italic'}>
+      <Typography tag={'h1'} typo={'heading1'} type={'italic'}>
         Typography
       </Typography>
-      <Typography typo={'heading_2'} color={'green'}>
+      <Typography tag={'h2'} typo={'heading2'} type={'italic'}>
         Typography
       </Typography>
-      <Button size={'l'}>Large</Button>
-      <Button size={'m'}>Medium</Button>
-      <Button size={'s'}>Small</Button>
-      <Button size={'xs'}>Xsmall</Button>
-      <br />
-      <Button size={'xl'} bgcolor={'white'}>
-        Xlarge
-      </Button>
-      <Button size={'l'} bgcolor={'white'}>
-        Large
-      </Button>
-      <Button size={'m'} bgcolor={'white'}>
-        Medium
-      </Button>
-      <Button size={'s'} bgcolor={'white'}>
-        Small
-      </Button>
-      <Button size={'xs'} bgcolor={'white'}>
-        <Icon size={'xs'} icon={'arrow-right'} />
-        Xsmall
-      </Button>
-      <br />
-      <Icon size={'xl'} icon={'plus'} />
-      <Icon size={'s'} icon={'plus'} />
-      <Icon size={'xl'} icon={'arrow-down'} />
-      <Icon size={'xl'} icon={'check'} />
+      <Typography typo={'text_xxl'}>Typography</Typography>
+      <Typography tag={'p'} typo={'text_s'}>
+        Typography
+      </Typography>
     </div>
   );
 };
